7036569ed012f64eb8d0b27872d2d6c6bee53bd1
Samuel Furtado Fortes <samuelfurtado@fadex.org.br>
2025-11-10 20:32:04 +0000

    remova esse botão, esse sistema nao vai trabalhar com cadastro de coorde

--- src/app/(app)/coordenadores/page.tsx
+++ src/app/(app)/coordenadores/page.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@/components/ui/button";
+
 import { PageHeader } from "@/components/page-header";
 import { DataTable } from "@/components/data-table/data-table";
 import { columns } from "@/components/data-table/columns-coordenadores";
@@ -7,9 +7,7 @@ import { coordinators } from "@/lib/data";
 export default function CoordenadoresPage() {
   return (
     <>
-      <PageHeader title="Coordenadores">
-        <Button>Cadastrar Novo Coordenador</Button>
-      </PageHeader>
+      <PageHeader title="Coordenadores" />
       <div className="p-4 sm:p-6 bg-card rounded-lg shadow-sm">
         <DataTable columns={columns} data={coordinators} />
       </div>
