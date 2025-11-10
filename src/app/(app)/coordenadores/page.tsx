448af40713b6a80c02e6f51cf9779b14e522e78f
Samuel Furtado Fortes <samuelfurtado@fadex.org.br>
2025-11-10 20:33:11 +0000

    Try fixing this error: `Console Error: params are being enumerated. `par

--- src/app/(app)/coordenadores/page.tsx
+++ src/app/(app)/coordenadores/page.tsx
@@ -4,13 +4,21 @@ import { DataTable } from "@/components/data-table/data-table";
 import { columns } from "@/components/data-table/columns-coordenadores";
 import { coordinators } from "@/lib/data";
 
+// Componente que busca os dados e renderiza a tabela
+function CoordinatorsTable() {
+  const data = coordinators;
+  return (
+    <div className="p-4 sm:p-6 bg-card rounded-lg shadow-sm">
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
+}
+
 export default function CoordenadoresPage() {
   return (
     <>
       <PageHeader title="Coordenadores" />
-      <div className="p-4 sm:p-6 bg-card rounded-lg shadow-sm">
-        <DataTable columns={columns} data={coordinators} />
-      </div>
+      <CoordinatorsTable />
     </>
   );
 }
